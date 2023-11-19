923da0ac2712fbde74de61fc28462f8171c7e354
ErikSvensson76 <eosvensson@gmail.com>
2023-11-19 15:46:27 +0100

    Initial commit

---
+++ src/main/java/com/example/oauth2resourceserversetup/Oauth2ResourceServerSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2resourceserversetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ResourceServerSetupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ResourceServerSetupApplication.class, args);
+    }
+
+}
